a1651ab36f9552af024a56ddaee0817da50459d2
Björn Dahlgren <bjodah@gmail.com>
2016-08-08 16:38:08 +0200

    Add sym to extras_require

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,6 +9,7 @@ for PYTHON in python2.7 python3.4; do
     (cd dist/; $PYTHON -m pip install $1-$($PYTHON ../setup.py --version).tar.gz)
     (cd /; $PYTHON -m pytest --pyargs $1)
     $PYTHON -m pip install --user -e .[all]
+    $PYTHON -m pip install --user symcxx pysym
 done
 PYTHONPATH=$(pwd) PYTHON=python2.7 ./scripts/run_tests.sh
 PYTHONPATH=$(pwd) PYTHON=python3.4 ./scripts/run_tests.sh --cov $1 --cov-report html
