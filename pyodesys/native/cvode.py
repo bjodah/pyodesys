b225be685bc071af009220837029a14bb94904c6
Björn Dahlgren <bjodah@gmail.com>
2017-02-20 15:34:15 +0100

    Tweak native standalone

--- pyodesys/native/cvode.py
+++ pyodesys/native/cvode.py
@@ -33,18 +33,16 @@ class NativeCvodeSys(_NativeSysBase):
     _NativeCode = NativeCvodeCode
     _native_name = 'cvode'
 
-    def as_standalone(self, outdir=None, compile_kwargs=None):
+    def as_standalone(self, out_file=None, compile_kwargs=None):
         from pycompilation.compilation import src2obj, link
         from pycodeexport.util import render_mako_template_to
-        outdir = outdir or '.'
         compile_kwargs = compile_kwargs or {}
         impl_src = open([f for f in self._native._written_files if f.endswith('.cpp')][0], 'rt').read()
-        f = render_mako_template_to(os.path.join(os.path.dirname(__file__),
-                                                 'sources/standalone_template.cpp'),
-                                    'standalone.cpp', {'p_odesys': self, 'p_odesys_impl': impl_src})
+        f = render_mako_template_to(
+            os.path.join(os.path.dirname(__file__), 'sources/standalone_template.cpp'),
+            '%s.cpp' % out_file, {'p_odesys': self, 'p_odesys_impl': impl_src})
         kw = copy.deepcopy(self._native.compile_kwargs)
         kw.update(compile_kwargs)
-        print(kw)
         objf = src2obj(f, **kw)
         kw['libraries'].append('boost_program_options')
-        return link([objf], **kw)
+        return link([objf], out_file, **kw)
--- pyodesys/native/sources/standalone_template.cpp
+++ pyodesys/native/sources/standalone_template.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <type_traits>
+#include <boost/exception/diagnostic_information.hpp>
 #include <boost/program_options.hpp>
 #include <cvodes_anyode_parallel.hpp>
 
@@ -19,16 +20,20 @@ int main(int argc, char *argv[]){
         ("rtol", po::value<realtype>()->default_value(1e-8), "relative tolerance")
         ("mxsteps", po::value<int>()->default_value(500), "maximum number of steps")
         ("lmm", po::value<std::string>()->default_value("BDF"), "linear multistep method")
-        ("return_on_root", po::value<bool>()->default_value(false), "Return on root")
+        ("return-on-root", po::value<bool>()->default_value(false), "Return on root")
         ("autorestart", po::value<int>()->default_value(0), "Autorestart (autonomous)")
-        ("return_on_error", po::value<bool>()->default_value(false), "Return on error")
-        ("get_dx_max_factor", po::value<realtype>()->default_value(1.0), "get_dx_max multiplicative factor")
-        ("error_outside_bounds", po::value<bool>()->default_value(false), "Return recoverable error to solver when outside bounds")
-        ("special_settings", po::value<std::string>()->default_value(""), "special settings2 (if customized)")
+        ("return-on-error", po::value<bool>()->default_value(false), "Return on error")
+        ("get-dx-max-factor", po::value<realtype>()->default_value(1.0), "get_dx_max multiplicative factor")
+        ("error-outside-bounds", po::value<bool>()->default_value(false), "Return recoverable error to solver when outside bounds")
+        ("special-settings", po::value<std::string>()->default_value(""), "special settings2 (if customized)")
         ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
+    try {
+        po::notify(vm);
+    } catch (boost::exception& exc) {
+        std::cerr << boost::diagnostic_information(exc);
+    }
 
     if (vm.count("help")) {
         std::cout << desc << "\n";
@@ -59,15 +64,15 @@ int main(int argc, char *argv[]){
     const int maxl=0;
     const realtype eps_lin=0.0;
     const unsigned nderiv=0;
-    bool return_on_root(vm["return_on_root"].as<bool>());
+    bool return_on_root(vm["return-on-root"].as<bool>());
     int autorestart(vm["autorestart"].as<int>());
-    bool return_on_error(vm["return_on_error"].as<bool>());
+    bool return_on_error(vm["return-on-error"].as<bool>());
 
     std::vector<realtype> params;
-    const realtype get_dx_max_factor(vm["get_dx_max_factor"].as<realtype>());
-    bool error_outside_bounds = vm["error_outside_bounds"].as<bool>();
+    const realtype get_dx_max_factor(vm["get-dx-max-factor"].as<realtype>());
+    bool error_outside_bounds = vm["error-outside-bounds"].as<bool>();
     std::vector<realtype> special_settings;
-    auto special_settings_stream = std::istringstream(vm["special_settings"].as<std::string>());
+    auto special_settings_stream = std::istringstream(vm["special-settings"].as<std::string>());
     for (std::string item; std::getline(special_settings_stream, item, ',');){
         special_settings.push_back(std::atof(item.c_str()));
     }
@@ -216,5 +221,5 @@ int main(int argc, char *argv[]){
 
     for (auto& v : systems)
         delete v;
-    return 0;
+    return EX_OK;
 }
