d8bbef826425e7d200e8ca664ff9150d3914418a
Björn Dahlgren <bjodah@gmail.com>
2017-09-05 11:21:21 +0200

    Don't run slow and veryslow tests during conda build

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 echo "Running tests..."
-CPLUS_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib MPLBACKEND=agg LLAPACK=openblas python -m pytest --verbose --pyargs pyodesys --slow --veryslow
+CPLUS_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib MPLBACKEND=agg LLAPACK=openblas python -m pytest --verbose --pyargs pyodesys
