0b197468fdc9bb478284ac5daa241005159c028a
Björn Dahlgren <bjodah@gmail.com>
2017-10-02 13:18:19 +0200

    Skip standalone notebook for now

--- scripts/render_notebooks.sh
+++ scripts/render_notebooks.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 cd examples/
-jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 *.ipynb
+for ipynb in *.ipynb; do
+    if [[ $ipynb == "_native_standalone.ipynb" ]]; then
+        continue  # issue with boost's program options
+    fi
+    jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 $ipynb
+done
 ../scripts/render_index.sh *.html
