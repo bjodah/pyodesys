36b30ae1b465dd8662cea8bb7fa395c206b6a910
Björn Dahlgren <bjodah@gmail.com>
2018-05-02 12:00:14 +0200

    Adapt post release script to noarch:python

--- scripts/post_release.sh
+++ scripts/post_release.sh
@@ -25,10 +25,8 @@ sed -i -E \
     dist/conda-recipe-$VERSION/meta.yaml
 
 ssh $PKG@$SERVER 'mkdir -p ~/public_html/conda-packages'
-for CONDA_PY in 27 35 36; do
-    anfilte-build . dist/conda-recipe-$VERSION dist/ --python ${CONDA_PY}
-    scp dist/linux-64/${PKG}-${VERSION}-py${CONDA_PY}*.bz2 $PKG@$SERVER:~/public_html/conda-packages/
-done
+anfilte-build . dist/conda-recipe-$VERSION dist/
+scp dist/linux-64/${PKG}-${VERSION}*.bz2 $PKG@$SERVER:~/public_html/conda-packages/
 ssh $PKG@$SERVER 'mkdir -p ~/public_html/conda-recipes'
 scp -r dist/conda-recipe-$VERSION/ $PKG@$SERVER:~/public_html/conda-recipes/
 scp "$SDIST_FILE" "$PKG@$SERVER:~/public_html/releases/"
