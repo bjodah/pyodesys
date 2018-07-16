4b64dc7f196a65ad74c611394acfcd8513a04c21
Sean P. Cornelius <spcornelius@gmail.com>
2018-07-15 21:12:21 -0400

    Add native code test for jtimes

--- pyodesys/native/tests/test_cvode.py
+++ pyodesys/native/tests/test_cvode.py
@@ -213,3 +213,26 @@ def test__PartiallySolvedSystem_Native():
 @requires('sym', 'pycvodes')
 def test_chained_parameter_variation_native_cvode():
     _test_chained_parameter_variation(NativeSys.from_other)
+
+
+@pytest.mark.veryslow
+@requires('sym', 'pycvodes')
+def test_jtimes_native_cvode(nu=0.01, k=1.0, m=1.0, x0=1.0, atol=1.0e-12, rtol=1.0e-12):
+    # Damped harmonic oscillator
+    w0 = (k/m)**0.5
+    def f(t, y, p):
+        return [y[1], -w0**2 * y[0] - nu * y[1]]
+
+    odesys = NativeSys.from_callback(f, 2, 0, jac=True, jtimes=True)
+    tout, yout, info = odesys.integrate(100, [x0, 0], integrator='native', with_jtimes=True,
+                                        with_jacobian=False, method='bdf', linear_solver='gmres',
+                                        atol=atol, rtol=rtol, nsteps=20000)
+
+    w = (w0**2 - nu**2/4.0)**0.5
+    a = (x0**2 + (nu * x0/2)**2/w**2)**0.5
+    phi = np.arctan(nu * x0 / (2 * x0 * w))
+    ref = a * np.exp(-nu * tout/2) * np.cos(w * tout - phi)
+    assert info['njvev'] > 0
+    assert info['njev'] == 0
+    assert np.allclose(yout[:,0], ref)
+
