ba8b4fd376d827b70d3335a2e57d0e1b5812c16a
spcornelius <spcornelius@gmail.com>
2018-07-31 15:51:26 -0400

    Change integrator from 'native' -> 'cvode' in jtimes test

--- pyodesys/native/tests/test_cvode.py
+++ pyodesys/native/tests/test_cvode.py
@@ -225,7 +225,7 @@ def test_jtimes_native_cvode(nu=0.01, k=1.0, m=1.0, x0=1.0, atol=1.0e-12, rtol=1
         return [y[1], -w0**2 * y[0] - nu * y[1]]
 
     odesys = NativeSys.from_callback(f, 2, 0, jac=True, jtimes=True)
-    tout, yout, info = odesys.integrate(100, [x0, 0], integrator='native', with_jtimes=True,
+    tout, yout, info = odesys.integrate(100, [x0, 0], integrator='cvode', with_jtimes=True,
                                         with_jacobian=False, method='bdf', linear_solver='gmres',
                                         atol=atol, rtol=rtol, nsteps=20000)
 
