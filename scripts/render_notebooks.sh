4e6aaac561671d880fab8f4a0f389b0f3aa042c9
spcornelius <spcornelius@gmail.com>
2018-08-09 09:46:28 -0500

    Skip rendering of certain notebook in extended prec.

--- scripts/render_notebooks.sh
+++ scripts/render_notebooks.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 cd examples/
+
+PREC = $(shell python3 -c "from pycvodes._config import env; print(env.get('SUNDIALS_PRECISION', 'double'))")
+
 for ipynb in *.ipynb; do
     if [[ $ipynb == "_native_standalone.ipynb" ]]; then
         continue  # issue with boost's program options
+    fi
+    if [[ $PREC != "double" && $ipynb == "_robertson.ipynb" ]]; then
+        continue
     fi
     jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 $ipynb
 done
