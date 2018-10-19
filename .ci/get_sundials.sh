35a501de29afeeabe3cdafa6b40681451467050f
Björn Dahlgren <bjodah@gmail.com>
2018-10-19 11:41:54 +0200

    Tweak py2 test

--- .ci/get_sundials.sh
+++ .ci/get_sundials.sh
@@ -18,6 +18,9 @@ elif [[ $VERSION == "3.1.2" ]]; then
 elif [[ $VERSION == "3.2.0" ]]; then
     SUNDIALS_FNAME="sundials-3.2.0.tar.gz"
     SUNDIALS_MD5="669e05565d3294478848ed497ac35a6e"
+elif [[ $VERSION == "3.2.1" ]]; then
+    SUNDIALS_FNAME="sundials-3.2.1.tar.gz"
+    SUNDIALS_MD5="65c42e4fec7d1f4f4bcd670f9bbe31c0"
 elif [[ $VERSION == "2.7.0" ]]; then
     SUNDIALS_FNAME="sundials-2.7.0.tar.gz"
     SUNDIALS_MD5="c304631b9bc82877d7b0e9f4d4fd94d3"
---
+++ .ci/test_py2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export PKG_NAME=$1
+
+for p in "${@:2}"
+do
+export CPATH=$p/include:$CPATH LIBRARY_PATH=$p/lib:$LIBRARY_PATH LD_LIBRARY_PATH=$p/lib:$LD_LIBRARY_PATH
+done
+
+
+( python2 -m pip >/dev/null || curl -Ls https://bootstrap.pypa.io/get-pip.py | python2 - --user )
+python2 -m pip install --user virtualenv
+python2 -m virtualenv /tmp/test_py2
+sed -i -E -e "/python_requires/d" setup.py
+bash -c "source /tmp/test_py2/bin/activate; pip install pytest '.[all]' && pytest -rs --pyargs ${PKG_NAME}"
+
