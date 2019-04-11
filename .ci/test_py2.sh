8819f96f3a23b6fe2b5f6728fef300c5277f9173
Björn Dahlgren <bjodah@gmail.com>
2019-04-11 13:52:28 +0200

    tweak CI setup

--- .ci/test_py2.sh
+++ .ci/test_py2.sh
@@ -8,7 +8,6 @@ export CPATH=$p/include:$CPATH LIBRARY_PATH=$p/lib:$LIBRARY_PATH LD_LIBRARY_PATH
 done
 
 
-( python2 -m pip >/dev/null || curl -Ls https://bootstrap.pypa.io/get-pip.py | python2 - --user )
 python2 -m pip install --user virtualenv
 python2 -m virtualenv /tmp/test_py2
 sed -i -E -e "/python_requires/d" setup.py
