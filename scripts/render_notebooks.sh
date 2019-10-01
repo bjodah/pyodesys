9d8b8e91e59a7eeede6fba4da7c9b267909addbb
Björn Ingvar Dahlgren <bjodah@gmail.com>
2019-10-01 16:05:35 +0200

    bump pycvodes req. version & CI: suppress notebook output

--- scripts/render_notebooks.sh
+++ scripts/render_notebooks.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -e
+QUIET_EXIT_CODE=0
+function quiet_unless_fail {
+    # suppresses function output unless exit status is != 0
+    OUTPUT_FILE=$(tempfile)
+    #/bin/rm --force /tmp/suppress.out 2>/dev/null
+    EXECMD=${1+"$@"}
+    $EXECMD > ${OUTPUT_FILE} 2>&1
+    QUIET_EXIT_CODE=$?
+    if [ ${QUIET_EXIT_CODE} -ne 0 ]; then
+	cat ${OUTPUT_FILE}
+	echo "The following command exited with exit status ${QUIET_EXIT_CODE}: ${EXECMD}"
+    fi
+    /bin/rm ${OUTPUT_FILE}
+}
+
+
 cd examples/
 
 PREC=`python3 -c "from pycvodes._config import env; print(env.get('SUNDIALS_PRECISION', 'double'))"`
@@ -10,6 +26,10 @@ for ipynb in *.ipynb; do
     if [[ $PREC != "double" && $ipynb == "_robertson.ipynb" ]]; then
         continue
     fi
-    jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=900 $ipynb
+    quiet_unless_fail jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=900 "${ipynb}" \
+        | grep -v -e "^\[NbConvertApp\] content: {'data':.*'image/png'"
+    if [ ${QUIET_EXIT_CODE} -ne 0 ]; then
+        exit ${QUIET_EXIT_CODE}
+    fi
 done
 ../scripts/render_index.sh *.html
