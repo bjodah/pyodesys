7e28a85a15a3c761d3f550bf4488611755eb0a60
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-04-21 23:45:00 +0200

    use pycvodes-0.13.0

--- .ci/run_ci.sh
+++ .ci/run_ci.sh
@@ -14,8 +14,9 @@ mkdir -p $HOME/.config/pip/
 echo -e "[global]\nno-cache-dir = false\ndownload-cache = $(pwd)/ci_cache/pip_cache" >$HOME/.config/pip/pip.conf
 python3 -m pip install symcxx pysym  # unofficial backends, symengine is tested in the conda build
 
-(cd ./tmp/pycvodes; CFLAGS="-isystem $SUNDBASE/include $CFLAGS" LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib $LDFLAGS" python3 setup.py install)
-git clean -e tmp/ -xfd
+# (cd ./tmp/pycvodes;
+CFLAGS="-isystem $SUNDBASE/include $CFLAGS" LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib $LDFLAGS" python3 -m pip install pycvodes # setup.py install )
+git clean -xfd # -e tmp/
 
 export CPATH=$SUNDBASE/include
 export LIBRARY_PATH=$SUNDBASE/lib
